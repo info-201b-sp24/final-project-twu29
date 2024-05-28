f179cff0dfdef050efbf896420f06d2149c683ce
Alley Wu <165843534+twu29@users.noreply.github.com>
2024-05-28 15:12:29 -0700

    upload

---
+++ clean dataset.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+sleep_dataset <- read_csv("info final/Sleep_health_and_lifestyle_dataset.csv")
+
+sleep_dataset <- sleep_dataset %>% 
+  select(Gender, Age, `Quality of Sleep`, `Physical Activity Level`, `Sleep Disorder`)
+
+write.csv(sleep_dataset, "sleep_dataset.csv", row.names = F)
